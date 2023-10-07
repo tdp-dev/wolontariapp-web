620c7e02799b7efba498f2ca32b7c42bbb976c9e
Roland Sobczak <rolandsobczak@icloud.com>
2023-10-07 13:39:17 +0200

    feat: pipeline output folder changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from '@routes/Home';
-import Settings from '@routes/Settings';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import '@styles/index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "@routes/Home";
+import Settings from "@routes/Settings";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@styles/index.scss";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: 'settings/',
-    element: <Settings />,
-  },
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "settings/",
+        element: <Settings />,
+    },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
