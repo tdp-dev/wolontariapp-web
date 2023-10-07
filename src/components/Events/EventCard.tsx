279d2eb1833c424dbd435d66b4d6014269b1baf9
Roland Sobczak <rolandsobczak@icloud.com>
2023-10-07 23:56:42 +0200

    feat: event cards connected to API

--- src/components/Events/EventCard.tsx
+++ src/components/Events/EventCard.tsx
@@ -1,11 +1,36 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Button from '@components/Button';
 import { Route, Routes } from 'react-dom/client';
 import EventPage from './EventPage';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-export default function EventCard() {
+interface EventCardProps {
+  title: string;
+  description: string;
+  date: string;
+  image: string;
+}
+
+export default function EventCard({ title, description, date, image }: EventCardProps) {
   const navigate = useNavigate();
+  const [imageUri, setImageUri] = useState(null);
+
+  const fetchImageHandler = async (imageUrl: string) => {
+    const { data } = await axios.get(imageUrl, {
+      responseType: 'blob',
+    });
+    let reader = new window.FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = function (event) {
+      let imageDataUrl = event.target.result;
+      setImageUri(imageDataUrl);
+    };
+  };
+
+  useEffect(() => {
+    fetchImageHandler(image);
+  }, []);
 
   const onClick = useCallback(() => {
     navigate(`/events/1`);
@@ -13,15 +38,14 @@ export default function EventCard() {
 
   return (
     <div className='event-card'>
-      <div className='event-card__image'></div>
+      <div className='event-card__image'>
+        <img src={imageUri} alt='event image' />
+      </div>
 
       <div className='event-card__text'>
-        <h3 className='event-card__title'>Dolore laboris Lorem sint</h3>
-        <span className='event-card__date'>12.20.2023 - 20.12.2023</span>
-        <p className='event-card__description'>
-          nulla cillum nostrud et consectetur fugiat id deserunt elit ex enim est fugiat do exercitation culpa mollit mollit consequat duis nostrud ipsum deserunt occaecat in labore mollit officia non
-          quis qui ad fugiat id
-        </p>
+        <h3 className='event-card__title'>{title}</h3>
+        <span className='event-card__date'>{date}</span>
+        <p className='event-card__description'>{description}</p>
 
         <div className='event-card__button'>
           <Button content='Zarządzaj' onClick={onClick}></Button>
--- src/components/Events/EventList.tsx
+++ src/components/Events/EventList.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import EventCard from './EventCard';
+import API from 'src/helpers/apiConnector';
 
 export default function EventList() {
-  return (
-    <div className='event-list'>
-      <EventCard></EventCard>
-      <EventCard></EventCard>
-      <EventCard></EventCard>
-      <EventCard></EventCard>
-    </div>
-  );
+  const [events, setEvents] = useState([]);
+
+  const fetchEvents = useCallback(async () => {
+    const { data } = await API.get('/events');
+    setEvents(data);
+  }, []);
+
+  const eventCards = useMemo(() => {
+    return events.map(({ id, title, description, image }) => <EventCard key={id} title={title} description={description} image={image} />);
+  }, [events]);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  return <div className='event-list'>{eventCards}</div>;
 }
