8694e22bf60afc30ee31883ae9fb05f9a876af04
Maciek <maciej.zuk@owlsdepartment.com>
2023-10-08 08:21:05 +0200

    feature: added notifications

--- src/main.tsx
+++ src/main.tsx
@@ -34,8 +34,8 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <React.StrictMode>
+  <>
     <Header></Header>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </>,
 );
